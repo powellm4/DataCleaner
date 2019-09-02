abbcefa5c028756612c35d4e3849826d0f431f11
Marshall Powell <powellmarshall3@gmail.com>
2019-09-01 23:32:05 -0400

    created all classes csv

--- functions.py
+++ functions.py
@@ -1,3 +1,6 @@
+import os
+import pandas as pd
+import numpy as np
 
 def is_instructor_name_df(df):
 	if 'Total for' in df.iloc[0][0]:
@@ -24,11 +27,22 @@ def get_instructor_name_for_group(master_list, group):
 	return name
 
 
-def remove_fluff_from_master_list(master_list):
+def remove_fluff_dfs_from_master_list(master_list):
 	new_master = []
 	for index, df in enumerate(master_list):
 		if 'Vitalidad Movement Arts Center' not in df.iloc[0][0]:
 			new_master.append(df)
+	return new_master
+
+
+def remove_totals_dfs_from_master_list(master_list):
+	new_master = []
+	for index, df in enumerate(master_list):
+		if 'Total for' not in df.iloc[0][0] \
+				and '# Clients' not in df.columns\
+				and 'Grand total' not in df.iloc[0][0]:
+			new_master.append(df)
+
 	return new_master
 
 
@@ -41,7 +55,75 @@ def move_header_row_to_top_of_data_frame(master_list, group):
 		df.drop(df.tail(2).index, inplace=True)
 
 
-def append_instructor_name_as_column(master_list, group):
+def append_instructor_name_as_column(master_list, group, instructor_name):
 	list_length = len(group)
 	for i in range(0, list_length):
-		group[i]
+		df = master_list[group[i]]
+		df = df.assign(Instructors=instructor_name)
+		master_list[group[i]] = df
+
+
+# def write_group_to_csv(master_list, group, instructor_name):
+
+def write_master_list_to_csv(master_list):
+	for df in master_list:
+		if os.path.isfile("%s%s.csv" % ('output/', '00-01-Class-All')):
+			mode = "a"
+			include_header = False
+		else:
+			mode = "w"
+			include_header = True
+		df.to_csv("%s%s.csv" % ('output/', '00-01-Class-All'), mode=mode, header=include_header, index=False)
+
+
+# creates the [folder] inside the /output/ folder
+def create_folder(folder):
+    if not os.path.exists('output/'):
+        os.mkdir('output/')
+    if not os.path.exists(folder):
+        os.mkdir(folder)
+
+
+def format_column_headers(df):
+    df.columns = [c.strip() for c in df.columns]
+    df.columns = [c.replace(' ', '_') for c in df.columns]
+    df.columns = [c.replace('.', '') for c in df.columns]
+    df.columns = [c.replace('"', '') for c in df.columns]
+    df.columns = [c.replace('Appointment', 'Class') for c in df.columns]
+    df.columns = [c.replace('Appt', 'Class') for c in df.columns]
+    return df
+
+
+def format_client_name(df):
+    df.Client_Name = df.Client_Name.str.replace(" ", "")
+    return df
+
+
+def sort_by_date_time(df):
+    df.Class_Date = pd.to_datetime(df.Class_Date)
+    return df.sort_values(by=['Class_Date', 'Class_Time'])
+
+def remove_quotes(df):
+    df = df.apply(lambda x: x.str.strip())
+    return df.apply(lambda x: x.str.strip('"'))
+
+
+def drop_nan_columns(df):
+	df = df.dropna(axis='columns', how='all')
+
+	# for idx in df.columns._nan_idxs:
+	# 	df = df.drop([1, idx])
+	return df
+
+def drop_unnecessary_columns(df):
+	if "Unnamed:_5" in df.columns:
+		df = df.drop(columns=["Unnamed:_5"])
+	if "Earnings_per_Client" in df.columns:
+		df = df.drop(columns=["Earnings_per_Client"])
+	if "Earnings" in df.columns:
+		df = df.drop(columns=["Earnings"])
+	if "Revenue" in df.columns:
+		df = df.drop(columns=["Revenue"])
+	if "Rev_per_Session" in df.columns:
+		df = df.drop(columns=["Rev_per_Session"])
+	return df
--- main.py
+++ main.py
@@ -1,16 +1,33 @@
 import pandas
 from functions import *
 
+create_folder('csvs')
 initial_read = pandas.read_html('Payroll Report 7-15-2019 - 7-31-2019.xls')
-master_list = remove_fluff_from_master_list(initial_read)
+master_list = remove_fluff_dfs_from_master_list(initial_read)
 initial_read = None
 instructor_df_groups = group_data_frames_by_instructor(master_list)
 
 for group in instructor_df_groups:
 	instructor_name = get_instructor_name_for_group(master_list, group)
 	move_header_row_to_top_of_data_frame(master_list, group)
-	# append_instructor_name_as_column(master_list, group)
+	append_instructor_name_as_column(master_list, group, instructor_name)
+	# write_group_to_csv(master_list, group, instructor_name)
 
+
+master_list = remove_totals_dfs_from_master_list(master_list)
+
+
+for index, df in enumerate(master_list):
+	df = drop_nan_columns(df)
+	df = format_column_headers(df)
+	df = drop_unnecessary_columns(df)
+	df = remove_quotes(df)
+	df = format_client_name(df)
+	df = sort_by_date_time(df)
+	master_list[index] = df
+
+write_master_list_to_csv(master_list)
+print('x')
 
 
 
@@ -27,19 +44,21 @@ for group in instructor_df_groups:
 	
 		
 	for each df list
-		---last data frame in list has instr name, get from there
-		---remove the VMAC Total df
-		--move header from bottom to top of dataframe
-		prepend new column = instructor name
-		determine whether public or private?
-		create file in format -> 00-Class-instructor .csv
+		-X-last data frame in list has instr name, get from there
+		-X-remove the VMAC Total df
+		-X-move header from bottom to top of dataframe
+		-X-prepend new column = instructor name
+		-X-remove Total df from master_list
+		-X-write master list to one csv
+		---look up pandas read html for colspan=3 causing 3 duplicate columns
+		---combine all group df's into 1 dataframe 
+		---create file in format -> 01-Class-instructor.csv
 		
 		
 	approach: no longer distinguish between public and private.
-		add the instructor name as a new column at the begining of data frame
+		add the instructor name as a new column at the beginning of data frame
 		every df goes into its own file
 		do data column clean up here. replace appointment with class etc.
 		
 '''
 
-print('x')
